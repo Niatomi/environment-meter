182eaa863de787a4825427e2ca5d44faf33c4eaa
Niatomi <playervoker@gmail.com>
2022-06-27 22:12:29 +0400

    Implemented logic for C02 and TDS sensors

--- arduino-hardware/sketch.ino
+++ arduino-hardware/sketch.ino
@@ -1,139 +1,117 @@
-// // #include <MHZ.h>
-// #include <LiquidCrystal_I2C.h>
-// #define DS18B20 A0 
-// #define LM35 A2 
-// #define TDS A1
-// #define CO2 A3 
+// #include <MHZ.h>
+#include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
 
-// LiquidCrystal_I2C lcd (0x27, 20, 4);
+/* PH Meter using PH Sensor
+*
+* https://bestengineeringprojects.com/arduino-ph-meter-using-ph-sensor/
+*
+*/
+#define T1 A2 
+#define T2 5
+// #define P0 A0
 
+/*
+* C02 Meter
+*
+* https://itnan.ru/post.php?c=2&p=272090
+*/
+#define CO2 A3
+SoftwareSerial mySerial(A0, A1); // TX, RX
+unsigned int ppm = 0;
+byte cmd[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79}; 
+unsigned char response[9];
 
-// void setup() {
-//     Serial.begin(9600);
+/*
+* TDS
+*
+* http://wiki.amperka.ru/products:troyka-tds-sensor
+*/
+#define TDS A2
+float tdsSensor;
 
-//     pinMode(TDS, INPUT);
-//     pinMode(TDS, INPUT);
-//     pinMode(LM35, INPUT);
-//     pinMode(DS18B20, INPUT);
-    
-//     lcd.init();
-//     lcd.backlight();
-// }
+LiquidCrystal_I2C lcd (0x27, 20, 4);
 
-// void loop() {
-//     // https://bestengineeringprojects.com/arduino-ph-meter-using-ph-sensor/
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
 
-//     // CO2 sensor
-//     // http://emariete.com/en/sensor-co2-mh-z19b/#El_sensor_de_CO_MH-Z19B
+  pinMode(TDS, INPUT);
+  pinMode(T1, INPUT);
+  pinMode(T2, INPUT);
 
-//     lcd.setCursor(0, 0);
-//     lcd.print(analogRead(CO2));
-//     // lcd.setCursor(0, 1);
-//     // lcd.print(analogRead(DS18B20));
-//     delay(250);
-//     lcd.clear();
-// }
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
 
-
-// CO2 Sensor
-
-// #include <SoftwareSerial.h>
-// SoftwareSerial co2Serial(7, 6); // define MH-Z19 RX TX
-// unsigned long startTime = millis();
- 
-// void setup() {
-//   Serial.begin(9600);
-//   co2Serial.begin(9600);
-//   pinMode(9, INPUT);
-// }
- 
-// void loop() {
-//   Serial.println("------------------------------");
-//   Serial.print("Time from start: ");
-//   Serial.print((millis() - startTime) / 1000);
-//   Serial.println(" s");
-//   int ppm_uart = readCO2UART();
-//   int ppm_pwm = readCO2PWM();
-//   delay(5000);
-// }
-
-// int readCO2UART(){
-//   byte cmd[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79};
-//   byte response[9]; // for answer
-
-//   Serial.println("Sending CO2 request...");
-//   co2Serial.write(cmd, 9); //request PPM CO2
-
-//   // clear the buffer
-//   memset(response, 0, 9);
-//   int i = 0;
-//   while (co2Serial.available() == 0) {
-// //    Serial.print("Waiting for response ");
-// //    Serial.print(i);
-// //    Serial.println(" s");
-//     delay(1000);
-//     i++;
-//   }
-//   if (co2Serial.available() > 0) {
-      co2Serial.readBytes(response, 9);
-  }
-  // print out the response in hexa
-  for (int i = 0; i < 9; i++) {
-    Serial.print(String(response[i], HEX));
-    Serial.print("   ");
-  }
-  Serial.println("");
-
-  // checksum
-  byte check = getCheckSum(response);
-  if (response[8] != check) {
-    Serial.println("Checksum not OK!");
-    Serial.print("Received: ");
-    Serial.println(response[8]);
-    Serial.print("Should be: ");
-    Serial.println(check);
-  }
-  
-  // ppm
-  int ppm_uart = 256 * (int)response[2] + response[3];
-  Serial.print("PPM UART: ");
-  Serial.println(ppm_uart);
-
-  // temp
-  byte temp = response[4] - 40;
-  Serial.print("Temperature? ");
-  Serial.println(temp);
-
-  // status
-  byte status = response[5];
-  Serial.print("Status? ");
-  Serial.println(status); 
-  if (status == 0x40) {
-    Serial.println("Status OK"); 
-  }
-  
-  return ppm_uart;
 }
 
-byte getCheckSum(char *packet) {
-  byte i;
-  unsigned char checksum = 0;
-  for (i = 1; i < 8; i++) {
-    checksum += packet[i];
-  }
-  checksum = 0xff - checksum;
-  checksum += 1;
-  return checksum;
+void loop() {
+  getPPM();
+  getPPMCO2();
+
+  printDataOnLcd();
+  delay(10000);
 }
 
-int readCO2PWM() {
-  unsigned long th, tl, ppm_pwm = 0;
-  do {
-    th = pulseIn(9, HIGH, 1004000) / 1000;
-    tl = 1004 - th;
-    ppm_pwm = 5000 * (th-2)/(th+tl-4);
-  } while (th == 0);
-  Serial.print("PPM PWM: ");
-  Serial.println(ppm_pwm);
-  return ppm_pwm;  
+/*
+*
+* 350 — 450 ppm: Нормальный уровень на открытом воздухе.
+*
+* < 600 ppm: Приемлемые уровни. Уровень. рекомендованный для спален, детских садов и школ.
+*
+* 600 — 1000 ppm: Жалобы на несвежий воздух, возможно снижение концентрации внимания.
+*
+* 1000 ppm: Максимальный уровень стандартов ASHRAE 
+* (American Society of Heating, Refrigerating and Air-Conditioning Engineers) и 
+* OSHA (Occupational Safety & Health Administration).
+*
+* 1000 — 2500 ppm: Общая вялость, снижение концентрации внимания, возможна головная боль.
+*
+* 2500 — 5000 ppm: Возможны нежелательные эффекты на здоровье.
+* 
+*/
+void getPPM() 
+{
+  mySerial.write(cmd, 9);
+  memset(response, 0, 9);
+  mySerial.readBytes(response, 9);
+  int i;
+  byte crc = 0;
+  for (i = 1; i < 8; i++) crc+=response[i];
+  crc = 255 - crc;
+  crc++;
+
+  if ( !(response[0] == 0xFF && response[1] == 0x86 && response[8] == crc) ) {
+    Serial.println("CRC error: " + String(crc) + " / "+ String(response[8]));
+  } else {
+    unsigned int responseHigh = (unsigned int) response[2];
+    unsigned int responseLow = (unsigned int) response[3];
+    ppm = (256*responseHigh) + responseLow;
+  }
+}
+
+void printDataOnLcd() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  // lcd.print("C02: ");
+  lcd.print(ppm);
+  // lcd.setCursor(9, 0);
+  // lcd.print("ppm");
+
+  lcd.setCursor(0, 1);
+  // lcd.print("TDS: ");
+  lcd.print(tdsSensor);
+  // lcd.setCursor(9, 1);
+  // lcd.print("ppm");
+
+}
+
+void getPPMCO2() {
+
+  int valueSensor = analogRead(TDS);
+  float voltageSensor = valueSensor * 5 / 1024.0;
+  tdsSensor = (133.42 * pow(voltageSensor, 3) - 255.86 * pow(voltageSensor, 2) + 857.39 * voltageSensor) * 0.5;
+
 }
