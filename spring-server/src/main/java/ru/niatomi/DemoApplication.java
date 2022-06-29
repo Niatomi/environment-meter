c8f54b46fc1ef85c5831de84122170fb4bbbf7f7
Niatomi <playervoker@gmail.com>
2022-06-30 00:45:19 +0400

    Create class for check logic

--- spring-server/src/main/java/ru/niatomi/DemoApplication.java
+++ spring-server/src/main/java/ru/niatomi/DemoApplication.java
@@ -12,11 +12,15 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         LocalDateTime now = LocalDateTime.now();
+        LocalDateTime now1 = LocalDateTime.now().plusWeeks(1L);
 
         Object local;
 //        Correct for
-        long unixTime2 = now.toEpochSecond(ZoneOffset.of("+04:00"));
+        long unixTime1 = now.toEpochSecond(ZoneOffset.of("+04:00"));
+        long unixTime2 = now1.toEpochSecond(ZoneOffset.of("+04:00"));
+        System.out.println(unixTime1);
         System.out.println(unixTime2);
+        System.out.println(unixTime2 - unixTime1);
     }
 
 }
