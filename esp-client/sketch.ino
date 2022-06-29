7f66dd997d3c98c6d8d732daa0625e6e04c368a1
Niatomi <playervoker@gmail.com>
2022-06-29 18:01:44 +0400

    Add Serial communication with Arduino

--- esp-client/sketch.ino
+++ esp-client/sketch.ino
@@ -1,22 +1,32 @@
+#include <ArduinoJson.h>
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>        
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include "WiFiManager.h"          
 #include <SPI.h> 
+#include <WiFiClient.h>
+#include <TimeLib.h>
 
 #define HTTP_LED D1
 #define SPI_LED D2
 
 void configModeCallback (WiFiManager *myWiFiManager) {
-  Serial.println("Entered config mode");
-  Serial.println(WiFi.softAPIP());
+  WiFi.softAPIP();
   //if you used auto generated SSID, print it
-  Serial.println(myWiFiManager->getConfigPortalSSID());
+  myWiFiManager->getConfigPortalSSID();
 }
 
+WiFiClient client; 
+
+String regionID = "51"; 
+
 void setup() {
+  Serial.end();
   Serial.begin(9600);
+  Serial.flush();
+  Serial.setDebugOutput(false); 
+   
 
   pinMode(HTTP_LED, OUTPUT);
   pinMode(SPI_LED, OUTPUT);
@@ -25,32 +35,34 @@ void setup() {
 
   digitalWrite(HTTP_LED, false);
   digitalWrite(SPI_LED, false);
-
-  SPI.begin();
-  char buff[] = "WCI\n";
-  for(int i=0; i<sizeof buff; i++) {
-    SPI.transfer(buff[i]);
-  }
-
   
-  // WiFiManager wifiManager;
+  WiFiManager wifiManager;
+  wifiManager.setDebugOutput(false);
   
   //reset settings - for testing
   // wifiManager.resetSettings();
 
   //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
-  // wifiManager.setAPCallback(configModeCallback);
+  wifiManager.setAPCallback(configModeCallback);
+  Serial.setDebugOutput(false);
+  Serial.flush();
+  Serial.println("-WiFiConfigStart-");
 
   //fetches ssid and pass and tries to connect
   //if it does not connect it starts an access point with the specified name
   //here  "AutoConnectAP"
   //and goes into a blocking loop awaiting configuration
-  // if(!wifiManager.autoConnect("ESP8266 ConfigMe")) {
-  //   Serial.println("failed to connect and hit timeout");
-  //   //reset and try again, or maybe put it to deep sleep
-  //   ESP.reset();
-  //   delay(1000);
-  // } 
+  if (!wifiManager.autoConnect("ESP8266 ConfigMe")) {
+
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(1000);
+  } 
+
+  // String SSID = wifiManager.getWiFiSSID();
+  // String password = wifiManager.getWiFiPass();
+
+  Serial.println("-WiFiConfigEnd-");
 
   //if you get here you have connected to the WiFi
   // Serial.println(F("WIFIManager connected!"));
@@ -71,5 +83,4 @@ void setup() {
 }
 
 void loop() {
-
 }
