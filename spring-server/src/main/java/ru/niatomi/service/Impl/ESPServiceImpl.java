a37c0aae378e3047498766dd7f737f558dd5d088
Niatomi <playervoker@gmail.com>
2022-07-03 03:02:13 +0400

    DI add

--- spring-server/src/main/java/ru/niatomi/service/Impl/ESPServiceImpl.java
+++ spring-server/src/main/java/ru/niatomi/service/Impl/ESPServiceImpl.java
@@ -1,14 +1,22 @@
 package ru.niatomi.service.Impl;
 
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.niatomi.mapper.ReferenceDataMapper;
+import ru.niatomi.mapper.SensorsMapper;
+import ru.niatomi.model.domain.sensor.Sensors;
+import ru.niatomi.model.domain.arduinoConfig.ReferenceData;
 import ru.niatomi.model.dto.ReferenceDataDto;
 import ru.niatomi.model.dto.SensorsDto;
 import ru.niatomi.model.dto.TimeScheduleDto;
+import ru.niatomi.repository.ReferenceDataRepository;
+import ru.niatomi.repository.SensorsRepository;
 import ru.niatomi.service.ESPService;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author niatomi
@@ -16,6 +24,12 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class ESPServiceImpl implements ESPService {
+
+    private final SensorsMapper sensorsMapper;
+    private final SensorsRepository sensorsRepository;
+
+    private final ReferenceDataRepository referenceDataRepository;
+    private final ReferenceDataMapper referenceDataMapper;
 
     @Override
     public ReferenceDataDto getReferenceData() {
