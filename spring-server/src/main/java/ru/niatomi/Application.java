e7ad690b5849172f3d9d9f281487f071f2d36286
Niatomi <playervoker@gmail.com>
2022-07-04 00:34:12 +0400

    Add command line runner for init of mongo persistence

--- spring-server/src/main/java/ru/niatomi/Application.java
+++ spring-server/src/main/java/ru/niatomi/Application.java
@@ -1,7 +1,12 @@
 package ru.niatomi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import ru.niatomi.model.domain.arduinoConfig.ReferenceData;
+import ru.niatomi.repository.ReferenceDataRepository;
 
 /**
  * @author niatomi
@@ -10,5 +15,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(ReferenceDataRepository referenceDataRepository) {
+        return args -> {
+            ReferenceData referenceData = new ReferenceData();
+            referenceData.setId(1);
+            referenceData.setVolume(5.5);
+            referenceData.setNormalPh(5.5);
+            referenceData.setC02(1500);
+            referenceData.setUpdated(false);
+            referenceDataRepository.save(referenceData);
+
+        };
+
     }
 }
