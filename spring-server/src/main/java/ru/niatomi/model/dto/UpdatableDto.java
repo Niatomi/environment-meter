540ae752a030a073cb2469c67c5cd5dd6293759a
Niatomi <playervoker@gmail.com>
2022-07-04 00:29:17 +0400

    Initial commit

---
+++ spring-server/src/main/java/ru/niatomi/model/dto/UpdatableDto.java
@@ -0,0 +1,19 @@
+package ru.niatomi.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author niatomi
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UpdatableDto {
+
+    private boolean isUpdatable;
+
+}
