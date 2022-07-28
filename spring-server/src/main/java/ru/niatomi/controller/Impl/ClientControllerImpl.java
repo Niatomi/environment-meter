174b6e2cea23ed2c9133e5110f8f4409fe901d88
Niatomi <playervoker@gmail.com>
2022-07-28 19:53:01 +0400

    DI add to all controllers

--- spring-server/src/main/java/ru/niatomi/controller/Impl/ClientControllerImpl.java
+++ spring-server/src/main/java/ru/niatomi/controller/Impl/ClientControllerImpl.java
@@ -1,6 +1,7 @@
 package ru.niatomi.controller.Impl;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,31 +12,38 @@ import ru.niatomi.model.dto.data.DataDto;
 import ru.niatomi.model.dto.time.ExactTimeDto;
 import ru.niatomi.model.dto.time.TimePlanAndExactDto;
 import ru.niatomi.model.dto.time.TimePlanDto;
+import ru.niatomi.service.ClientService;
+
+import java.util.List;
 
 /**
  * @author niatomi
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/client")
 public class ClientControllerImpl implements ClientController {
+
+    private final ClientService clientService;
+
     @Override
     public ResponseEntity<String> updateReferenceData(ReferenceDataDto referenceData) {
-        return null;
+        return ResponseEntity.ok(clientService.updateReferenceData(referenceData));
     }
 
     @Override
     public ResponseEntity<String> addExactTime(ExactTimeDto exactTimeDto) {
-        return null;
+        return ResponseEntity.ok(clientService.addExactTime(exactTimeDto));
     }
 
     @Override
     public ResponseEntity<String> addTimePlan(@RequestBody TimePlanDto timePlanDto) {
-        return null;
+        return ResponseEntity.ok(clientService.addTimePlan(timePlanDto));
     }
 
     @Override
     public ResponseEntity<TimePlanAndExactDto> getAllTime() {
-        return null;
+        return ResponseEntity.ok(clientService.getAllTime());
     }
 
     @Override
