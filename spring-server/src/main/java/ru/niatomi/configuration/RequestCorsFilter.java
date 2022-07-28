a1bb148df53a2ec6e236e6131ccb7a009226ffae
Niatomi <playervoker@gmail.com>
2022-07-28 21:00:36 +0400

    Add cors filter

---
+++ spring-server/src/main/java/ru/niatomi/configuration/RequestCorsFilter.java
@@ -0,0 +1,34 @@
+package ru.niatomi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Arrays.*;
+
+/**
+ * @author niatomi
+ */
+@Configuration
+public class RequestCorsFilter {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+
+}
