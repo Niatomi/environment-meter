30bff734a8222288330ddef91d30454849fd3851
Niatomi <playervoker@gmail.com>
2022-08-01 17:10:49 +0400

    Imported charts module

--- angular-client/angular-clinet/src/app/app.module.ts
+++ angular-client/angular-clinet/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { LineChartComponent } from './line-chart/line-chart.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LineChartComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgApexchartsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
