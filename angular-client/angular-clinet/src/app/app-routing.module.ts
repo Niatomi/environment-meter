7dc130dc097d5bf37e6078c4d337d49c7681f44b
Niatomi <playervoker@gmail.com>
2022-08-01 17:10:34 +0400

    Implemented router module

--- angular-client/angular-clinet/src/app/app-routing.module.ts
+++ angular-client/angular-clinet/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LineChartComponent } from './line-chart/line-chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sensors/:name', component: LineChartComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
